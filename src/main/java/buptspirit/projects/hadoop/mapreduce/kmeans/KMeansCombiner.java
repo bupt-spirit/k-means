785d90386306d280aca4e6799accf67f32b5f9b7
Gui Li <894780761@qq.com>
2018-11-11 19:12:33 +0800

    finish combiner

--- src/main/java/buptspirit/projects/hadoop/mapreduce/kmeans/KMeansCombiner.java
+++ src/main/java/buptspirit/projects/hadoop/mapreduce/kmeans/KMeansCombiner.java
@@ -1,6 +1,9 @@
 package buptspirit.projects.hadoop.mapreduce.kmeans;
 
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
@@ -15,5 +18,20 @@ public class KMeansCombiner extends Reducer<LongWritable, DoubleArrayWritable, L
     @Override
     protected void reduce(LongWritable key, Iterable<DoubleArrayWritable> values, Context context) throws IOException, InterruptedException {
         super.reduce(key, values, context);
+        int dimension = values.iterator().next().get().length;
+        DoubleWritable[] sum = new DoubleWritable[dimension];
+        int count = 0;
+        for(DoubleArrayWritable array: values){
+            String[] vals = array.toString().split(" ");
+            for(int i=0; i<dimension; i++){
+                sum[i] = new DoubleWritable(Double.valueOf(sum[i].toString())+Double.valueOf(vals[i]));
+            }
+            count+=1;
+        }
+
+        DoubleArrayWritable result = new DoubleArrayWritable();
+        result.set(sum);
+
+        PartialResult partialResult = new PartialResult(key, result);
     }
 }
